9acd6c18902d16a19df4db9717c30c2880cd9a08
vahbib <singhbibhav084@gmail.com>
2020-09-25 21:36:32 +0530

    Added Start

---
+++ Start.java
@@ -0,0 +1,8 @@
+package LineComparisonProblem;
+
+public class Start {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
+	}
+
+}
